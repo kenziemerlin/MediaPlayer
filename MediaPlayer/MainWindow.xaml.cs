86896870dcae517f96dbbabfea2434a2c2b7f31f
kenziemerlin <kenziemerlin01@gmail.com>
2024-02-05 23:29:33 -0400

    Add project files.

---
+++ MediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayerApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TagLib.File currentFile;
+        MediaPlayer myMediaPlayer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            myMediaPlayer = new MediaPlayer();
+        }
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            //example of instantiating open fie dialog
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //show onscreen to usr
+            if (ofd.ShowDialog() == true)
+            {
+                mediaMenuItem.Visibility = Visibility.Visible;
+
+                currentFile = TagLib.File.Create(ofd.FileName);
+                MediaPlayerUC media = new MediaPlayerUC(currentFile.Tag.Title, currentFile.Tag.FirstPerformer, currentFile.Tag.Album, currentFile.Tag.Year, null);
+
+                MediaContent.Children.Add(media);
+            }
+        }
+
+        private void Play_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            myMediaPlayer.Open(new Uri(currentFile.Name));
+            myMediaPlayer.Play();
+        }
+
+        private void Pause_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            myMediaPlayer.Pause();
+        }
+
+        private void Stop_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            myMediaPlayer.Stop();
+        }
+    }
+}
---
+++ MediaPlayer/MediaPlayerUC.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaPlayerApplication
+{
+    /// <summary>
+    /// Interaction logic for MediaPlayerUC.xaml
+    /// </summary>
+    public partial class MediaPlayerUC : UserControl
+    {
+
+        public string? SongTitle { get; set; }
+        public string? Artist { get; set; }
+        public string? Album { get; set; }
+        public uint? Year { get; set; }
+
+        public Image? image { get; set; }
+
+        public MediaPlayerUC()
+        {
+            InitializeComponent();
+        }
+
+        public MediaPlayerUC(string songTitle, string artist, string album, uint year, Image? myImage)
+        {
+            SongTitle = songTitle;
+            Artist = artist;
+            Album = album;
+            Year = year;
+            image = myImage;
+
+            InitializeComponent();
+
+            TitleLabel.Content = this.SongTitle;
+            ArtistLabel.Content = this.Artist;
+            AlbumLabel.Content = this.Album;
+            YearLabel.Content = this.Year;
+            
+
+
+        }
+    }
+}
